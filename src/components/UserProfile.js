a6a922d5f243574bb8718b9a42e8b1e115321e9a
Utkarsh Dubey <utk23dubey@gmail.com>
2021-09-16 16:53:14 +0530

    Changes in User Profile

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,9 +1,10 @@
-import classes from './UserProfile.module.css';
+import classes from "./UserProfile.module.css";
 
 const UserProfile = () => {
   return (
     <main className={classes.profile}>
       <h2>My User Profile</h2>
+      <h3 className={classes.value}>Utkarsh Dubey</h3>
     </main>
   );
 };
